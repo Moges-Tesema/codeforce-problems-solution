7bc869b3d47cc1225bd285daeb6826e95871ce73
Moges-Tesema <moges2421@gmail.com>
2025-02-12 18:24:57 +0300

    organize solutions in subfolder for the sake of structuring data and solve two easy codeforce problem

--- soldierAndBanana.py
+++ soldierAndBanana.py
@@ -2,5 +2,7 @@ firstCost, dollar,bananas = list(map(int,input().split()))
 cost=0
 for i in range(1,bananas+1):
     cost+= i*firstCost
-
-print(cost-dollar)
+if dollar >= cost:
+    print(0)
+else:
+    print(cost-dollar)
